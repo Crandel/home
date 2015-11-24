83a7c47ee5d75a5c396e8e15f88f47e154486fb1
Crandel <cradlemann@gmail.com>
2015-11-24 21:24:04 +0200

    fish function extract

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -25,6 +25,43 @@ function upl
 end
 # End Docker
 
+function extract
+    if set -q argv
+        switch $argv
+            case '*.tar.bz2'
+                tar -xvjf $argv
+            case '*.tar.gz'
+                tar -xvzf $argv
+            case '*.tar.xz'
+                tar -xvJf $argv
+            case '*.bz2'
+                bunzip2 $argv
+            case '*.rar'
+                unrar x $argv
+            case '*.gz'
+                gunzip $argv
+            case '*.tar'
+                tar xvf $argv
+            case '*.tbz2'
+                tar xvjf $argv
+            case '*.tgz'
+                tar xvzf $argv
+            case '*.zip'
+                unzip $argv
+            case '*.Z'
+                uncompress $argv
+            case '*.7z'
+                7z x $argv
+            case '*.xz'
+                unxz $argv
+            case '*.exe'
+                cabextract $argv
+            case '*'
+                echo $argv ": unrecognized file compression"
+        end
+    end
+end
+
 function backup
    cd /opt/work/backup/$argv
 end
