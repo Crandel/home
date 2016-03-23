be92e6051a967c4efafe8f282a5bf0cb47c82241
Crandel <cradlemann@gmail.com>
2016-03-23 20:41:09 +0200

    fix keybindings

--- .emacs.d/settings/my_keybindings.el
+++ .emacs.d/settings/my_keybindings.el
@@ -1,26 +1,27 @@
 ;; Custom keybindings
 (global-set-key "\C-c\C-d" "\C-a\C- \C-n\M-w\C-y")
-;; Moving
+;; Movingi
+;;UP
 (global-unset-key (kbd "M-i"))
 (global-set-key (kbd "M-i") 'previous-line)
-
+;; LEFT
 (global-unset-key (kbd "M-j"))
 (global-set-key (kbd "M-j") 'backward-char)
-
+;; DOWN
 (global-unset-key (kbd "M-k"))
 (global-set-key (kbd "M-k") 'next-line)
 (global-set-key (kbd "C-c k") 'kill-sentence)
-
-(global-unset-key (kbd "M-i"))
-(global-set-key (kbd "M-i") 'previous-line)
+;; RIGHT
+(global-unset-key (kbd "M-l"))
+(global-set-key (kbd "M-l") 'forward-char)
 (global-set-key (kbd "C-c l") 'downcase-word)
 
 ;; Multiple cursors
-(global-set-key (kbd "C-c n") 'mc/mark-next-like-this)
-(global-set-key (kbd "C-c w") 'mc/mark-next-like-this-word)
-(global-set-key (kbd "C-c b") 'mc/mark-previous-like-this)
-(global-set-key (kbd "C-c e") 'mc/mark-previous-word-like-this)
-(global-set-key (kbd "C-c a") 'mc/mark-all-like-this)
+(global-set-key (kbd "C-c .") 'mc/mark-next-like-this-word) ; choose same word next
+(global-set-key (kbd "C-c ,") 'mc/mark-previous-word-like-this) ; choose same word previous
+(global-set-key (kbd "C-c n") 'mc/mark-next-like-this) ; choose char from next line same position
+(global-set-key (kbd "C-c m") 'mc/mark-previous-like-this); choose char from previous line same position
+(global-set-key (kbd "C-c /") 'mc/mark-all-like-this)
 ;; Magit
 (global-set-key (kbd "C-x C-z") 'magit-status)
 
