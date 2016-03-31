edcd600684a635f96685bb0633a0f8a0fe4dd02e
Crandel <cradlemann@gmail.com>
2016-03-31 12:21:16 +0300

    Change flycheck and neotree for better experience

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -3,16 +3,3 @@
 (require 'my_package)
 (require 'my_hooks)
 (require 'my_keybindings)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(neo-show-hidden-files t)
- '(neo-theme (quote arrow)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(neo-vc-added-face ((t (:foreground "brightyellow")))))
