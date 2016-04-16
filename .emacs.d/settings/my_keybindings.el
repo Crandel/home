e37e7e52d91f1f9ff204f1c928a314b5f75aff8a
Crandel <cradlemann@gmail.com>
2016-04-16 11:51:35 +0300

    Emacs keybindings

--- .emacs.d/settings/my_keybindings.el
+++ .emacs.d/settings/my_keybindings.el
@@ -39,8 +39,8 @@
 (global-set-key [M-S-down] 'move-line-down)
 
 ;; Multiple cursors
-(global-set-key (kbd "C-c >") 'mc/mark-next-like-this-word) ; choose same word next
-(global-set-key (kbd "C-c <") 'mc/mark-previous-word-like-this) ; choose same word previous
+(global-set-key (kbd "C-c <right>") 'mc/mark-next-like-this-word) ; choose same word next
+(global-set-key (kbd "C-c <left>") 'mc/mark-previous-word-like-this) ; choose same word previous
 (global-set-key (kbd "M-n") 'mc/mark-next-like-this) ; choose char from next line same position
 (global-set-key (kbd "M-m") 'mc/mark-previous-like-this); choose char from previous line same position
 (global-set-key (kbd "C-c C-_") 'mc/mark-all-like-this)
@@ -56,7 +56,8 @@
 
 ;; Helm
 (global-set-key (kbd "M-x") 'helm-M-x)
-(global-set-key (kbd "C-p") 'helm-mini)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
+(global-set-key (kbd "C-p") 'helm-multi-files)
 (global-set-key [f10] 'helm-semantic-or-imenu)
 (global-set-key (kbd "M-p") 'helm-projectile-ag)
 (global-set-key (kbd "M-y") 'helm-show-kill-ring)
