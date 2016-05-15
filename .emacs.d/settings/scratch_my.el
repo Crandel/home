0bd6ace2128da648f5ecdd232160bb14859251f3
Crandel <cradlemann@gmail.com>
2016-05-15 19:28:52 +0300

    disable electric mode

--- .emacs.d/settings/scratch_my.el
+++ .emacs.d/settings/scratch_my.el
@@ -37,7 +37,7 @@
 
 ;; Electric-modes settings
 (electric-pair-mode   -1) ;; автозакрытие {},[],() с переводом курсора внутрь скобок
-(electric-indent-mode  1) ;; отключить индентацию  electric-indent-mod'ом (default in Emacs-24.4)
+(electric-indent-mode -1) ;; отключить индентацию  electric-indent-mod'ом (default in Emacs-24.4)
 ;; Delete selection
 (delete-selection-mode t)
 
