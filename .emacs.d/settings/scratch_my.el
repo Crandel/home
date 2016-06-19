db6b87d2bbed8474a2bfc1c1e409349a69fd5ea7
Crandel <cradlemann@gmail.com>
2016-06-19 16:17:25 +0300

    Add markdown snippets

--- .emacs.d/settings/scratch_my.el
+++ .emacs.d/settings/scratch_my.el
@@ -171,5 +171,34 @@
 (if (fboundp 'global-font-lock-mode)
     (global-font-lock-mode 1))
 
+;; Russian
+(defun reverse-input-method (input-method)
+  "Build the reverse mapping of single letters from INPUT-METHOD."
+  (interactive
+   (list (read-input-method-name "Use input method (default current): ")))
+  (if (and input-method (symbolp input-method))
+      (setq input-method (symbol-name input-method)))
+  (let ((current current-input-method)
+        (modifiers '(nil (control) (meta) (control meta))))
+    (when input-method
+      (activate-input-method input-method))
+    (when (and current-input-method quail-keyboard-layout)
+      (dolist (map (cdr (quail-map)))
+        (let* ((to (car map))
+               (from (quail-get-translation
+                      (cadr map) (char-to-string to) 1)))
+          (when (and (characterp from) (characterp to))
+            (dolist (mod modifiers)
+              (define-key local-function-key-map
+                (vector (append mod (list from)))
+                (vector (append mod (list to)))))))))
+    (when input-method
+      (activate-input-method current))))
+
+(defadvice read-passwd (around my-read-passwd act)
+  (let ((local-function-key-map nil))
+    ad-do-it))
+
+ (reverse-input-method 'russian-typewriter)
 
 (provide 'scratch_my)
