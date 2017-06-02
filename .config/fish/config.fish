b27057fa51e2d2d23a0caa0044352aa869f2af03
Crandel <cradlemann@gmail.com>
2017-06-03 01:34:09 +0300

    add rust

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,48 +8,48 @@ set -q fish_data_path; or set -g fish_data_path $XDG_DATA_HOME/fish
 set -q fish_config_path; or set -g fish_config_path $XDG_CONFIG_HOME/fish
 
 if test -d /usr/lib/jvm/default
-	set -x JAVA_HOME /usr/lib/jvm/default
+  set -x JAVA_HOME /usr/lib/jvm/default
 else if test -d /usr/lib/jvm/default-java
-	set -x JAVA_HOME /usr/lib/jvm/default-java
+  set -x JAVA_HOME /usr/lib/jvm/default-java
 end
 
 if type -pq hadoop
-	set -x HADOOP_USER_NAME hadoop
+  set -x HADOOP_USER_NAME hadoop
 end
 
 if type -pq hive; and test -f /usr/lib/hive
-	set -x HIVE_HOME /usr/lib/hive
+  set -x HIVE_HOME /usr/lib/hive
 end
 
 if type -pq scala; and test -f /usr/share/scala
-	set -x SCALA_HOME /usr/share/scala
-	set -xg PATH $PATH $SCALA_HOME/bin
+  set -x SCALA_HOME /usr/share/scala
+  set -xg PATH $PATH $SCALA_HOME/bin
 end
 
 ## Functional variables
 if type -pq chromium
-	set -x BROWSER chromium
+  set -x BROWSER chromium
 else if type -pq chromium-browser
-	set -x BROWSER chromium-browser
+  set -x BROWSER chromium-browser
 end
 
 if type -pq rust
-	set -xg RUST $HOME/rust
+  set -xg RUST $HOME/rust
 end
 
 set -xg TERM "xterm-256color"
 if type -pq wine
-	set -xg WINEARCH "win32"
+  set -xg WINEARCH "win32"
 end
 
 set -x GNOME_DESKTOP_SESSION_ID 1
 
 if type -pq virtualenvwrapper.sh
-	set -x WORKON_HOME $HOME/.virtualenvs
+  set -x WORKON_HOME $HOME/.virtualenvs
 end
 
 if [ (python2 -c 'import pkgutil; print(1 if pkgutil.find_loader("virtualfish") else 0)') = "1" ]
-	eval (python2 -m virtualfish auto_activation global_requirements)
+  eval (python2 -m virtualfish auto_activation global_requirements)
 end
 
 set fish_greeting ""
@@ -62,251 +62,255 @@ set -g fish_cdhist_max 128
 
 # different checkers
 function sudo_run
-	if test (id -u) -eq 0
-		eval $argv
-	else
-		sudo $argv
-	end
+  if test (id -u) -eq 0
+    eval $argv
+  else
+    sudo $argv
+  end
 end
 
 if type -pq pacman; and test -f $fish_config_path/pacman.fish
-	source $fish_config_path/pacman.fish
+  source $fish_config_path/pacman.fish
 end
 
 if type -pq apt; and test -f $fish_config_path/apt.fish
-	source $fish_config_path/apt.fish
+  source $fish_config_path/apt.fish
 end
 
 if type -pq docker; and test -f $fish_config_path/docker.fish
-	source $fish_config_path/docker.fish
+  source $fish_config_path/docker.fish
 end
 
 if type -pq vagrant; and test -f $fish_config_path/vagrant.fish
-	source $fish_config_path/vagrant.fish
+  source $fish_config_path/vagrant.fish
 end
 
 if type -pq go; and test -f $fish_config_path/go.fish
-	source $fish_config_path/go.fish
+  source $fish_config_path/go.fish
 end
 
 function extract
-	if set -q argv
-		switch $argv
-			case '*.tar.bz2'
-				tar -xvjf $argv
-			case '*.tar.gz'
-				tar -xvzf $argv
-			case '*.tar.xz'
-				tar -xvJf $argv
-			case '*.bz2'
-				bunzip2 $argv
-			case '*.rar'
-				unrar x $argv
-			case '*.gz'
-				gunzip $argv
-			case '*.tar'
-				tar xvf $argv
-			case '*.tbz2'
-				tar xvjf $argv
-			case '*.tgz'
-				tar xvzf $argv
-			case '*.zip'
-				unzip $argv
-			case '*.Z'
-				uncompress $argv
-			case '*.7z'
-				7z x $argv
-			case '*.xz'
-				unxz $argv
-			case '*.exe'
-				cabextract $argv
-			case '*'
-				echo $argv ": unrecognized file compression"
-		end
-	end
+  if set -q argv
+    switch $argv
+      case '*.tar.bz2'
+        tar -xvjf $argv
+      case '*.tar.gz'
+        tar -xvzf $argv
+      case '*.tar.xz'
+        tar -xvJf $argv
+      case '*.bz2'
+        bunzip2 $argv
+      case '*.rar'
+        unrar x $argv
+      case '*.gz'
+        gunzip $argv
+      case '*.tar'
+        tar xvf $argv
+      case '*.tbz2'
+        tar xvjf $argv
+      case '*.tgz'
+        tar xvzf $argv
+      case '*.zip'
+        unzip $argv
+      case '*.Z'
+        uncompress $argv
+      case '*.7z'
+        7z x $argv
+      case '*.xz'
+        unxz $argv
+      case '*.exe'
+        cabextract $argv
+      case '*'
+        echo $argv ": unrecognized file compression"
+    end
+  end
 end
 
 function zipin
-	for f in *
-		switch $f
-			case '*zip'
-				echo $f
-			case '*'
-				zip -9 $f.zip $f
-				rm $f
-		end
-	end
+  for f in *
+    switch $f
+      case '*zip'
+        echo $f
+      case '*'
+        zip -9 $f.zip $f
+        rm $f
+    end
+  end
 end
 
 function backup
-	cd /opt/work/backup
-	if count $argv > /dev/null
-		cd $argv
-	end
+  cd /opt/work/backup
+  if count $argv > /dev/null
+    cd $argv
+  end
 end
 
 function home_pr
-	cd /opt/work/home
+  cd /opt/work/home
 end
 
 function group_fix
-	sudo_run grpck
-	sudo_run pwck
+  sudo_run grpck
+  sudo_run pwck
 end
 
 ## Aliases
 function ls --description 'List contents of directory'
-	set -l param --color=auto
-	if isatty 1
-		set param $param --indicator-style=classify
-	end
-	if [ (uname -o) = "GNU/Linux" ]
-		set param $param --group-directories-first
-	end
-	command ls $param $argv
+  set -l param --color=auto
+  if isatty 1
+    set param $param --indicator-style=classify
+  end
+  if [ (uname -o) = "GNU/Linux" ]
+    set param $param --group-directories-first
+  end
+  command ls $param $argv
 end
 
 function l
-	ls -CF $argv
+  ls -CF $argv
 end
 
 function la
-	ls -A $argv
+  ls -A $argv
 end
 
 function ll
-	ls -ahlF $argv
+  ls -ahlF $argv
 end
 
 function hm -d "Merge history from several shells"
-	history --merge
+  history --merge
 end
 
 set -l listpr "(ls $MY_PROJECTS_ROOT | string replace '/' \t)"
 function pr -d "project directory"
-	# if argv when go to directory
-	set -l path $MY_PROJECTS_ROOT
-	if count $argv > /dev/null
-		set -x path $path"/"$argv
-	end
-	cd $path
+  # if argv when go to directory
+  set -l path $MY_PROJECTS_ROOT
+  if count $argv > /dev/null
+    set -x path $path"/"$argv
+  end
+  cd $path
 end
 complete -c pr -a "$listpr"
 
 set -l slistpr "(ls $MY_PROJECTS_ROOT/scala | string replace '/' \t)"
 function spr
-	# if argv when go to directory
-	set -l path $MY_PROJECTS_ROOT/scala
-	if count $argv > /dev/null
-		set -x path $path"/"$argv
-	end
-	cd $path
+  # if argv when go to directory
+  set -l path $MY_PROJECTS_ROOT/scala
+  if count $argv > /dev/null
+    set -x path $path"/"$argv
+  end
+  cd $path
 end
 complete -c spr -a "$slistpr"
 
-set -l glistpr "(ls $MY_GO_PROJECTS_ROOT | string replace '/' \t)"
+set -l glistpr "(ls $MY_GO_PROJECTS_ROOT/src | string replace '/' \t)"
 function gpr
-	# if argv when go to directory
-	set -l path $MY_GO_PROJECTS_ROOT
-	if count $argv > /dev/null
-		set -x path $path"/src/"$argv
-		switch (echo $argv)
-			case go_chat
-				set -l check (docker inspect -f "{{.State.Running}}" postgres)
-				if [ $check = "false" ]
-					docker start postgres
-				end
-		end
-	end
-	cd $path
+  # if argv when go to directory
+  set -l path $MY_GO_PROJECTS_ROOT
+  if count $argv > /dev/null
+    set -x path $path"/src/"$argv
+    switch (echo $argv)
+      case go_chat
+        set -l check (docker inspect -f "{{.State.Running}}" postgres)
+        if [ $check = "false" ]
+          docker start postgres
+        end
+    end
+  end
+  cd $path
 end
 complete -c gpr -a "$glistpr"
 
 function rmv
-	sudo_run mv $argv /tmp
+  sudo_run mv $argv /tmp
 end
 
 function soff
-	sudo_run swapoff (swapon --noheadings --show=NAME) # /dev/sda4
+  sudo_run swapoff (swapon --noheadings --show=NAME) # /dev/sda4
 end
 
 function son
-	sudo_run swapon (swapon --noheadings --show=NAME) #/dev/mapper/xubuntu--vg-swap_1
+  sudo_run swapon (swapon --noheadings --show=NAME) #/dev/mapper/xubuntu--vg-swap_1
 end
 
 function systemctl
-	sudo_run systemctl $argv
+  sudo_run systemctl $argv
 end
 
 if type -pq tmux
-	function tm
-		tmux attach
-		tmux new
-	end
+  function tm
+    tmux attach
+    tmux new
+  end
 end
 
 if type -pq emacs
-	set -x EDITOR "emacs -nw"
-	function em
-		emacs -nw $argv
-	end
+  set -x EDITOR "emacs -nw"
+  function em
+    emacs -nw $argv
+  end
 
-	function sem
-		sudo_run emacs -nw $argv
-	end
+  function sem
+    sudo_run emacs -nw $argv
+  end
 else if type -pq vim
-	set -x EDITOR vim
+  set -x EDITOR vim
 end
 
 if type -pq mc
-	function smc
-		sudo_run -E mc
-	end
+  function smc
+    sudo_run -E mc
+  end
 end
 
 if type -pq git
-	function pll
-		git pull origin $argv
-	end
-	function psh
-		git push origin $argv
-	end
-	function gst
-		git status
-	end
-	function gco
-		git checkout $argv
-	end
-	function gadd
-		git add .
-	end
-	function gcmt
-		git commit -m "$argv"
-	end
+  function pll
+    git pull origin $argv
+  end
+  function psh
+    git push origin $argv
+  end
+  function gst
+    git status
+  end
+  function gco
+    git checkout $argv
+  end
+  function gadd
+    git add .
+  end
+  function gcmt
+    git commit -m "$argv"
+  end
 end
 
 function update_kernel
-	sudo_run mkinitcpio -p linux
+  sudo_run mkinitcpio -p linux
+end
+
+if test -d /usr/src/rust
+  set -x RUST_SRC_PATH /usr/src/rust/src
 end
 
 if test -f $fish_config_path/local.fish
-	source $fish_config_path/local.fish
+  source $fish_config_path/local.fish
 end
 
 # start X at login
-# if status --is-login
-# 	if test -z "$DISPLAY" -a $XDG_VTNR -eq 1
-# 		exec /bin/bash startx -- -keeptty
-# 	end
-# end
+#if status --is-login
+#  if test -z "$DISPLAY" -a $XDG_VTNR -eq 1
+#    exec /bin/bash startx -- -keeptty
+#  end
+#end
 
 # if test -z "$DESKTOP_SESSION"
-#	eval (gnome-keyring-daemon --start)
-#	set -x SSH_AUTH_SOCK
+#   eval (gnome-keyring-daemon --start)
+#   set -x SSH_AUTH_SOCK
 # end
 
 
 function fish_title
-	echo $_ ' '
-	echo (prompt_pwd)
+  echo $_ ' '
+  echo (prompt_pwd)
 end
--- .emacs.d/settings/scratch_my.el
+++ .emacs.d/settings/scratch_my.el
@@ -1,30 +1,30 @@
 ;; Font settings
-(set-default-font "Hack 12")
+(set-default-font "Hack 16")
 ;; The full name of the user logged in
 ;; Full mailing address of user
-(setq-default user-full-name	 "crandel"
-							user-mail-adress "cradlemann@gmail.com")
+(setq-default user-full-name   "crandel"
+              user-mail-adress "cradlemann@gmail.com")
 
 ;; Set bash as default shell
-(setq shell-file-name						"/bin/bash"
-			explicit-shell-file-name	"/bin/bash")
+(setq shell-file-name             "/bin/bash"
+      explicit-shell-file-name  "/bin/bash")
 (blink-cursor-mode 0)
 ;; (setenv "GOPATH"
-;;	 (concat
-;;				 (getenv "HOME")
-;;				 "/go"))
+;;   (concat
+;;         (getenv "HOME")
+;;         "/go"))
 ;; (setenv "PATH"
-;;	 (concat
-;;		(getenv "PATH") ":"
-;;		(getenv "GOPATH") ":"
-;;		(getenv "GOPATH") "/bin"
-;;	 )
+;;   (concat
+;;    (getenv "PATH") ":"
+;;    (getenv "GOPATH") ":"
+;;    (getenv "GOPATH") "/bin"
+;;   )
 ;; )
 ;; (add-to-list 'exec-path (concat (getenv "GOPATH") "/bin"))
 
 ;; Inhibit startup/splash screen
-(setq inhibit-splash-screen		t
-			inhibit-startup-message t)
+(setq inhibit-splash-screen     t
+      inhibit-startup-message t)
 
 ;; Cursor
 (setq-default cursor-type 'bar)
@@ -32,85 +32,85 @@
 
 ;; Imenu
 (require 'imenu)
-(setq imenu-auto-rescan			 t
-			imenu-use-popup-menu	 nil)
+(setq imenu-auto-rescan        t
+      imenu-use-popup-menu   nil)
 (semantic-mode 1)
 
 ;; SavePlace
 (save-place-mode 1)
-(setq save-place-file											"~/.emacs.d/saved-places"
-			save-place-forget-unreadable-files		t)
+(setq save-place-file                       "~/.emacs.d/saved-places"
+      save-place-forget-unreadable-files    t)
 
 ;; Electric-modes settings
-(electric-pair-mode		-1)
-(electric-indent-mode	-1)
+(electric-pair-mode     -1)
+(electric-indent-mode   -1)
 ;; Delete selection
 (delete-selection-mode t)
 
 ;; Disable GUI components
-(tooltip-mode		-1)
-(menu-bar-mode		-1)
-(tool-bar-mode		-1)
-(menu-bar-mode		-1)
-(scroll-bar-mode	-1)
-(setq use-dialog-box				nil
-			redisplay-dont-pause	t
-			ring-bell-function		'ignore)
+(tooltip-mode     -1)
+(menu-bar-mode    -1)
+(tool-bar-mode    -1)
+(menu-bar-mode    -1)
+(scroll-bar-mode  -1)
+(setq use-dialog-box        nil
+      redisplay-dont-pause  t
+      ring-bell-function    'ignore)
 
 ;; Display the name of the current buffer in the title bar
 (setq frame-title-format "%b")
 
 ;; Disable backup/autosave files
-(setq backup-inhibited					t
-			make-backup-files				nil
-			auto-save-default				nil
-			auto-save-list-file-name nil)
+(setq backup-inhibited          t
+      make-backup-files         nil
+      auto-save-default         nil
+      auto-save-list-file-name nil)
 
 ;; Coding-system settings
-(set-language-environment							'UTF-8)
-(setq buffer-file-coding-system				'utf-8
-			file-name-coding-system					'utf-8)
-(setq-default coding-system-for-read		'utf-8)
-(set-selection-coding-system						'utf-8)
-(set-keyboard-coding-system						'utf-8-unix)
-(set-terminal-coding-system						'utf-8)
-(prefer-coding-system									'utf-8)
+(set-language-environment               'UTF-8)
+(setq buffer-file-coding-system         'utf-8
+      file-name-coding-system           'utf-8)
+(setq-default coding-system-for-read    'utf-8)
+(set-selection-coding-system            'utf-8)
+(set-keyboard-coding-system             'utf-8-unix)
+(set-terminal-coding-system             'utf-8)
+(prefer-coding-system                   'utf-8)
 
 ;; Linum plugin
 (require 'linum)
-(line-number-mode			t)
-(global-linum-mode		t)
-(column-number-mode		t)
-(setq linum-format		" %d")
+(line-number-mode       t)
+(global-linum-mode    t)
+(column-number-mode     t)
+(setq linum-format    " %d")
 
 ;; Fringe settings
 (fringe-mode '(8 . 0))
 (setq-default indicate-buffer-boundaries 'left)
 
 ;; Display file size/time in mode-line
-(setq display-time-24hr-format	t)
-(display-time-mode							t)
-(size-indication-mode						t)
+(setq display-time-24hr-format  t)
+(display-time-mode              t)
+(size-indication-mode             t)
 (defun add-mode-line-dirtrack ()
-	(add-to-list 'mode-line-buffer-identification
-							'(:propertize (" " default-directory " ") face dired-directory)))
+  (add-to-list 'mode-line-buffer-identification
+              '(:propertize (" " default-directory " ") face dired-directory)))
 (add-hook 'shell-mode-hook 'add-mode-line-dirtrack)
 
 ;; Indent settings
-(setq-default indent-tabs-mode			t
-							tab-width							2
-							tab-always-indent			nil
-							c-basic-offset				2
-							sh-basic-offset				2
-							scala-basic-offset		2
-							standart-indent				2
-							lisp-body-indent			2
-							indent-line-function	'insert-tab)
+(setq-default indent-tabs-mode      nil
+              tab-width               2
+              tab-always-indent       nil
+              c-basic-offset        2
+              sh-basic-offset         2
+              scala-basic-offset    2
+              standart-indent         2
+              lisp-body-indent      2
+              indent-line-function  'insert-tab)
 
 ;; Scrolling settings
-(setq scroll-step						1
-			scroll-margin					10
-			scroll-conservatively	10000)
+(setq scroll-step             1
+      scroll-margin           10
+      scroll-conservatively   10000)
 
 ;; Short messages
 (defalias 'yes-or-no-p 'y-or-n-p)
@@ -121,78 +121,78 @@
 (setq next-line-add-newlines nil)
 
 ;; Highlight search resaults
-(setq search-highlight					t
-			query-replace-highlight		t
-			auto-window-vscroll				nil)
-(setq bidi-display-reordering		nil)
+(setq search-highlight          t
+      query-replace-highlight     t
+      auto-window-vscroll         nil)
+(setq bidi-display-reordering     nil)
 ;;; Whitespace
 (require 'whitespace)
-(autoload 'global-whitespace-mode	"whitespace" "Toggle whitespace visualization." t)
+(autoload 'global-whitespace-mode   "whitespace" "Toggle whitespace visualization." t)
 (setq whitespace-style
-			'(face trailing spaces lines-tail empty indentation::tab indentation::space tabs newline space-mark tab-mark newline-mark))
+      '(face trailing spaces lines-tail empty indentation::tab indentation::space tabs newline space-mark tab-mark newline-mark))
 (global-whitespace-mode 1)
 (setq whitespace-display-mappings
-			;; all numbers are Unicode codepoint in decimal. ⁖ (insert-char 182 1)
-			'(
-				(space-mark 32 [183] [46]) ; 32 SPACE 「 」, 183 MIDDLE DOT 「·」, 46 FULL STOP 「.」
-				(newline-mark 10 [8617 10]) ; 10 LINE FEED
-				(lines-tail 10 [8617 10]) ; 10 LINE FEED
-				(tab-mark 9 [8594 9] [183 9]) ; 9 TAB, 9655 WHITE RIGHT-POINTING TRIANGLE 「▷」
-				)
-			whitespace-line-column 130)
+      ;; all numbers are Unicode codepoint in decimal. ⁖ (insert-char 182 1)
+      '(
+        (space-mark 32 [183] [46]) ; 32 SPACE 「 」, 183 MIDDLE DOT 「·」, 46 FULL STOP 「.」
+        (newline-mark 10 [8617 10]) ; 10 LINE FEED
+        (lines-tail 10 [8617 10]) ; 10 LINE FEED
+        (tab-mark 9 [8594 9] [183 9]) ; 9 TAB, 9655 WHITE RIGHT-POINTING TRIANGLE 「▷」
+        )
+      whitespace-line-column 130)
 
 (setq split-height-threshold nil
-			split-width-threshold	0)
+      split-width-threshold   0)
 
 (if (equal nil (equal major-mode 'org-mode))
-		(windmove-default-keybindings 'meta))
+    (windmove-default-keybindings 'meta))
 
 (recentf-mode 1)
-(setq recentf-max-menu-items			550
-			recentf-max-saved-items		550)
+(setq recentf-max-menu-items      550
+      recentf-max-saved-items     550)
 
 ;; Show paren
 (setq show-paren-delay 0
-			show-paren-style 'expression)
+      show-paren-style 'expression)
 (show-paren-mode 2)
 
-(setq ns-pop-up-frames					nil
-			ad-redefinition-action		'accept)
+(setq ns-pop-up-frames          nil
+      ad-redefinition-action    'accept)
 
 (if (fboundp 'global-font-lock-mode)
-		(global-font-lock-mode 1))
+    (global-font-lock-mode 1))
 
 ;; Russian
 (defun reverse-input-method (input-method)
-	"Build the reverse mapping of single letters from INPUT-METHOD."
-	(interactive
-	(list (read-input-method-name "Use input method (default current): ")))
-	(if (and input-method (symbolp input-method))
-			(setq input-method (symbol-name input-method)))
-	(let ((current current-input-method)
-				(modifiers '(nil (control) (meta) (control meta))))
-		(when input-method
-			(activate-input-method input-method))
-		(when (and current-input-method quail-keyboard-layout)
-			(dolist (map (cdr (quail-map)))
-				(let* ((to (car map))
-							(from (quail-get-translation
-											(cadr map) (char-to-string to) 1)))
-					(when (and (characterp from) (characterp to))
-						(dolist (mod modifiers)
-							(define-key local-function-key-map
-								(vector (append mod (list from)))
-								(vector (append mod (list to)))))))))
-		(when input-method
-			(activate-input-method current))))
+  "Build the reverse mapping of single letters from INPUT-METHOD."
+  (interactive
+  (list (read-input-method-name "Use input method (default current): ")))
+  (if (and input-method (symbolp input-method))
+      (setq input-method (symbol-name input-method)))
+  (let ((current current-input-method)
+        (modifiers '(nil (control) (meta) (control meta))))
+    (when input-method
+      (activate-input-method input-method))
+    (when (and current-input-method quail-keyboard-layout)
+      (dolist (map (cdr (quail-map)))
+        (let* ((to (car map))
+              (from (quail-get-translation
+                      (cadr map) (char-to-string to) 1)))
+          (when (and (characterp from) (characterp to))
+            (dolist (mod modifiers)
+              (define-key local-function-key-map
+                (vector (append mod (list from)))
+                (vector (append mod (list to)))))))))
+    (when input-method
+      (activate-input-method current))))
 
 ;(defadvice read-passwd (around my-read-passwd act)
-;	(let ((local-function-key-map nil))
-;		ad-do-it))
+;   (let ((local-function-key-map nil))
+;     ad-do-it))
 ;(reverse-input-method 'russian-typewriter)
 
-(setq max-mini-window-height		0.4
-			compilation-always-kill		t)
+(setq max-mini-window-height      0.4
+      compilation-always-kill     t)
 
 
 (provide 'scratch_my)
