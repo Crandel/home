46bbe52cdbaa6297ea5cc767d615c567c32150f6
Vitalii Drevenchuk <vitalii.drevenchuk@accenture.com>
2017-11-16 15:58:39 +0200

    last version of mongorc

--- .mongorc.js
+++ .mongorc.js
@@ -1,4 +1,4 @@
-// DBQuery.prototype._prettyShell = true
+DBQuery.prototype._prettyShell = true
 EDITOR="vim";
 
 var host = db.serverStatus().host.split('.')[0];
@@ -11,36 +11,10 @@ var current_db = db.getName();
 
 var prompt = function() {
   var def = username + "@" + host + "." + current_db + " > ";
-  var ndocs, obj, p, sec;
-  ndocs = function(n) {
-    return "" + n + " document" + (n > 1 ? 's' : '');
-  };
   db.setProfilingLevel(2);
-  obj = db.getLastErrorObj();
-  pq = db.system.profile.find({
-    $and: [
-      {ns: {$not: /profile$/}},
-      {ns: {$not: /\$cmd$/}},
-      {ns: {$not: /indexes$/}}
-    ]
-  }).sort({ts: -1}).limit(1);
-  if (pq.count() > 0){
-    var p = pq[0]
-    sec = "" + (p.millis / 1000) + " sec";
-    if (obj.err) {
-      return "Error: " + obj.err + "\n> ";
-    }
-    switch (p.op) {
-    case 'insert':
-      return "\nQuery OK, 1 document inserted (" + sec + ")\n" + def;
-    case 'query':
-      return "\n >= " + (ndocs(p.nreturned)) + " in set (" + sec + ")\n" + def;
-    case 'remove':
-      return "\nQuery OK, x documents removed (" + sec + ")\n" + def;
-    case 'update':
-      return "\nQuery OK, " + (ndocs(p.nupdated)) + " affected (" + sec + ")\n" + def;
-    }
+  var obj = db.getLastErrorObj();
+  if (obj.err) {
+    return "Error: " + obj.err + "\n> ";
   }
-
   return def;
 };
