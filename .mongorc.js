a16ac0ab7aec1fef2828b513e444cdb828203ae0
Vitalii Drevenchuk <vitalii.drevenchuk@accenture.com>
2017-11-17 16:36:27 +0200

    latest version of mongorc

--- .mongorc.js
+++ .mongorc.js
@@ -1,17 +1,27 @@
 DBQuery.prototype._prettyShell = true
 EDITOR="vim";
 
-var host = db.serverStatus().host.split('.')[0];
+var host = "@";
+var status = db.serverStatus();
+if (!!status){
+  host += status.host.split('.')[0];
+  host += ".";
+}
 var username = "anon";
-var user = db.runCommand({connectionStatus : 1}).authInfo.authenticatedUsers[0];
-if (!!user){
-  username = user.user;
+var connectSt = db.runCommand({connectionStatus : 1});
+if (!!connectSt){
+  var user = connectSt.authInfo.authenticatedUsers[0];
+  if (!!user){
+    username = user.user;
+  }
 }
 var current_db = db.getName();
 
 var prompt = function() {
-  var def = username + "@" + host + "." + current_db + " > ";
-  db.setProfilingLevel(2);
+  var def = username + host + current_db + " > ";
+  if(!!status){
+    db.setProfilingLevel(2);
+  }
   var obj = db.getLastErrorObj();
   if (obj.err) {
     return "Error: " + obj.err + "\n> ";
