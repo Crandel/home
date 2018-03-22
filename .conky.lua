c6ae5c357adc9f71ae868ec87f23105875c79284
Crandel <cradlemann@gmail.com>
2018-03-22 12:31:23 +0200

    conky fix

--- .conky.lua
+++ .conky.lua
@@ -5,7 +5,7 @@ conky.config = {
     background = false,
     max_text_width = 0,
     -- Update interval in seconds
-    update_interval = 2.0,
+    update_interval = 3.0,
     --[[
         This is the number of times Conky will update before quitting.
         Set to zero to run forever. ]]
@@ -31,9 +31,9 @@ conky.config = {
     cpu_avg_samples = 1,
     top_name_width = 8,
     -- Volume level
-    template0 = [[${exec pactl list sinks | grep 'Volume: front-left' -m 1 | awk '{print $5}'}]],
+    -- template0 = [[${exec pactl list sinks | grep 'Volume: front-left' -m 1 | awk '{print $5}'}]],
     -- Volume on/off
-    template1 = [[${exec pactl list sinks | grep 'Mute:' -m 1 | awk '{print $2}'}]],
+    -- template1 = [[${exec pactl list sinks | grep 'Mute:' -m 1 | awk '{print $2}'}]],
 };
 -- Stuff in text will be formatted on screen
 -- JSON for i3bar
@@ -41,30 +41,26 @@ conky.text = [[
 [
 { "full_text" : "\uF0E0 ${exec gmail}", "color" : "\#2E64FE"},
 { "full_text" : "\uF07C /${fs_free /}|h${fs_free /home}|d${fs_free /media/data}", "color" : "\#FA5882" },
-{ "full_text": "\uF1DE ${cpubar cpu0 }", "color" :
+{ "full_text": "\uF1DE ${cpubar}", "color" :
   ${if_match ${cpu}<90}
     "\#04B404"
   ${else}
     "\#FF0000"
   ${endif}
 },
-${if_match ${cpu}>90}
-{ "full_text" : "T ${top name 1}", "color" : "\#F0E68C", "separator_block_width": 1},
-${endif}
-${if_match ${memperc}>90}
-{ "full_text" : "T ${top name 1}", "color" : "\#F0E68C", "separator_block_width": 1},
-${endif}
-${if_match "$template1"=="no"}
-    { "full_text" : "\uF0F3$template0", "color": "\#E1F5A9"},
+{ "full_text" : "\uF0F3$pa_sink_volume%", "color": 
+${if_pa_sink_muted}
+"\#FF0000"
 ${else}
-    { "full_text" : "\uF1F6$template0", "color": "\#FF0000"},
+"\#E1F5A9"
 ${endif}
+},
 { "full_text" : "\uF1C0$mem" , "color" :
-  ${if_match ${memperc}<90}
-    "\#F7FE2E"
-  ${else}
-    "\#FF0000"
-  ${endif}
+${if_match ${memperc}<90}
+"\#F7FE2E"
+${else}
+"\#FF0000"
+${endif}
 },
 { "full_text" : "\uF17E $swapperc", "color" : "\#FE9A2E"},
 { "full_text" : "\uF205 $uptime_short", "color": "\#8B008B"},
