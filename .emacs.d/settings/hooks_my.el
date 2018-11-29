c5b5631c71b8baf5c7d561b8e59751f483bc7cac
Vitalii Drevenchuk <vitalii.drevenchuk@cognotekt.com>
2018-11-29 09:35:26 +0100

    small changes

--- .emacs.d/settings/hooks_my.el
+++ .emacs.d/settings/hooks_my.el
@@ -165,6 +165,7 @@
 (add-to-list 'auto-mode-alist '("\\.hql\\'" . sql-mode))
 (add-to-list 'auto-mode-alist '("\\.bashrc\\'" . sh-mode))
 (add-to-list 'auto-mode-alist '("\\.fish\\'" . fish-mode))
+(add-to-list 'auto-mode-alist '("Pipfile" . conf-toml-mode))
 (add-to-list 'auto-mode-alist '("docker-compose[^/]*\\.yml\\'" . docker-compose-mode))
 
 (provide 'hooks_my)
