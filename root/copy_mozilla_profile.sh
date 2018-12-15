fdf3add32ace5baa871c8af917c52254098e1a1d
Crandel <cradlemann@gmail.com>
2018-12-15 15:19:25 +0100

    Add debug info

--- root/copy_mozilla_profile.sh
+++ root/copy_mozilla_profile.sh
@@ -4,5 +4,9 @@ mozilla_profile_dir="/data/data/org.mozilla.firefox/files/mozilla/*.default"
 user_file="/storage/emulated/0/progs/termux/home/termux/user.js"
 
 cd $mozilla_profile_dir
+echo "$pwd"
 cp $user_file .
+echo "ls -la | grep user \n"
 chown `$(stat -c "%U:%G")` user.js
+echo "ls -la | grep user \n"
+
