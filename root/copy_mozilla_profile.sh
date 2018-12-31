edbdc09de00263f70b161942c514e0f731ee07ad
Crandel <cradlemann@gmail.com>
2018-12-31 19:42:41 +0100

    First try firefox update script

--- root/copy_mozilla_profile.sh
+++ root/copy_mozilla_profile.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 mozilla_profile_dir="/data/data/org.mozilla.firefox/files/mozilla/*.default"
-user_file="/storage/emulated/0/progs/termux/home/termux/user.js"
+user_files="/storage/emulated/0/progs/termux/home/termux/"
 
 cd $mozilla_profile_dir
 echo "$pwd"
-cp $user_file .
+cp -R $user_files .
 usernme=$(stat -c "%U" .)
 grp=$(stat -c "%G" .)
 echo "$(ls -la | grep --color=auto user.js)"
-chown "$usernme:$grp" user.js
+chown -R "$usernme:$grp" .
 echo "$(ls -la | grep  --color=auto user.js)"
 
