7983be74f7edc67838e449da5690db6d1a9d5443
Crandel <cradlemann@gmail.com>
2019-01-29 20:13:46 +0100

    Add termux props

--- root/copy_mozilla_profile.sh
+++ root/copy_mozilla_profile.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 
 mozilla_profile_dir="/data/data/org.mozilla.firefox/files/mozilla/*.default"
-user_file="/storage/emulated/0/progs/termux/home/termux/user.js"
-chrome="/storage/emulated/0/progs/termux/home/termux/chrome"
+user_files="/storage/emulated/0/progs/termux/home/termux/firefox/*"
 
 cd $mozilla_profile_dir
 echo "$pwd"
-cp -R $user_file .
-cp -R $chrome .
+cp -R $user_files .
 usernme=$(stat -c "%U" .)
 grp=$(stat -c "%G" .)
 echo "$(ls -la | grep --color=auto user.js)"
