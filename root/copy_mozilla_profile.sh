0b8ab3e0ea405779ed7d1384a323a7edafe30215
Crandel <cradlemann@gmail.com>
2019-01-29 19:58:12 +0100

    Firefox mobile telemetry

--- root/copy_mozilla_profile.sh
+++ root/copy_mozilla_profile.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
 mozilla_profile_dir="/data/data/org.mozilla.firefox/files/mozilla/*.default"
-user_files="/storage/emulated/0/progs/termux/home/termux/*"
+user_file="/storage/emulated/0/progs/termux/home/termux/user.js"
+chrome="/storage/emulated/0/progs/termux/home/termux/chrome"
 
 cd $mozilla_profile_dir
 echo "$pwd"
-cp -R $user_files .
+cp -R $user_file .
+cp -R $chrome .
 usernme=$(stat -c "%U" .)
 grp=$(stat -c "%G" .)
 echo "$(ls -la | grep --color=auto user.js)"
