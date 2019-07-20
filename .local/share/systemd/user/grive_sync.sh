17d04df2d30f846139d620eb727ea4208c857b09
Vitalii Drevenchuk <vitalii.drevenchuk@cognotekt.com>
2019-07-20 13:03:08 +0200

    i3 + grive

--- .local/share/systemd/user/grive_sync.sh
+++ .local/share/systemd/user/grive_sync.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-cd /media/data/work/backup/drive/
-grive -s keepass/
+cd <path>
+grive -s <folder>/
 retVal=$?
 if [ $retVal -ne 0 ]; then
     notify-send --urgency=critical 'Grive service' "Failed with service code $retVal"
+else
+    notify-send --urgency=normal 'Grive service' "Sync was successful"
 fi
