f19d2e483221dd9887aa8a09d1d057272acfa4ec
Vitalii Drevenchuk <vitalii.drevenchuk@cognotekt.com>
2019-08-07 13:33:36 +0200

    firefox settings with comments

--- termux/firefox/user.js
+++ termux/firefox/user.js
@@ -2,13 +2,21 @@ user_pref("accessibility.force_disabled", false);
 user_pref("app.normandy.enabled", false);
 user_pref("app.shield.optoutstudies.enabled", false);
 user_pref("app.update.checkInstallTime", false);
+// PREF: Disable "beacon" asynchronous HTTP transfers (used for analytics)
+// https://developer.mozilla.org/en-US/docs/Web/API/navigator.sendBeacon
+user_pref("beacon.enabled", false);
 user_pref("browser.discovery.enabled", false);
 user_pref("browser.bookmarks.restore_default_bookmarks", false);
+user_pref("browser.fixup.hide_user_pass", true);
 user_pref("browser.library.activity-stream.enabled", true);
+// PREF: Disable Extension recommendations (Firefox >= 65)
+// https://support.mozilla.org/en-US/kb/extension-recommendations
 user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.addons", false);
 user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.features", false);
 user_pref("browser.newtabpage.activity-stream.feeds.places", true);
 user_pref("browser.newtabpage.activity-stream.feeds.section.highlights", false);
+// PREF: Disable "Recommended by Pocket" in Firefox Quantum
+user_pref("browser.newtabpage.activity-stream.feeds.section.topstories", false);
 user_pref("browser.newtabpage.activity-stream.feeds.telemetry", false);
 user_pref("browser.newtabpage.activity-stream.filterAdult", false);
 user_pref("browser.newtabpage.activity-stream.prerender", true);
@@ -20,6 +28,10 @@ user_pref("browser.newtabpage.activity-stream.tippyTop.service.endpoint", "");
 user_pref("browser.newtabpage.activity-stream.topSitesRows", 3);
 user_pref("browser.newtabpage.enhanced", true);
 user_pref("browser.ping-centre.telemetry", true);
+// PREF: Disable Pocket
+// https://support.mozilla.org/en-US/kb/save-web-pages-later-pocket-firefox
+// https://github.com/pyllyukko/user.js/issues/143
+user_pref("browser.pocket.enabled", false);
 user_pref("browser.privatebrowsing.searchUI", false);
 user_pref("browser.safebrowsing.blockedURIs.enabled", false);
 user_pref("browser.safebrowsing.downloads.enabled", false);
@@ -55,14 +67,23 @@ user_pref("browser.safebrowsing.provider.mozilla.gethashURL", "");
 user_pref("browser.safebrowsing.provider.mozilla.lists", "");
 user_pref("browser.safebrowsing.provider.mozilla.pver", "");
 user_pref("browser.safebrowsing.provider.mozilla.updateURL", "");
+// PREF: Disable GeoIP lookup on your address to set default search engine region
+// https://trac.torproject.org/projects/tor/ticket/16254
+// https://support.mozilla.org/en-US/kb/how-stop-firefox-making-automatic-connections#w_geolocation-for-default-search-engine
 user_pref("browser.search.countryCode", "US");
+// PREF: Don't use Mozilla-provided location-specific search engines
 user_pref("browser.search.geoSpecificDefaults", false);
 user_pref("browser.search.geoSpecificDefaults.url", "");
 user_pref("browser.search.geoip.url", "");
 user_pref("browser.search.hiddenOneOffs", "Bing,Amazon.com,Twitter");
 user_pref("browser.search.region", "US");
 user_pref("browser.search.update", "false");
-user_pref("browser.send_pings", false);
+// PREF: Enable pinging URIs specified in HTML <a> ping= attributes
+// http://kb.mozillazine.org/Browser.send_pings
+user_pref("browser.send_pings", true);
+// PREF: When browser pings are enabled, only allow pinging the same host as the origin page
+// http://kb.mozillazine.org/Browser.send_pings.require_same_host
+user_pref("browser.send_pings.require_same_host", true);
 user_pref("browser.sessionstore.interval", 1800000);
 user_pref("browser.slowStartup.notificationDisabled", true);
 user_pref("browser.startup.page", 3);
@@ -74,21 +95,53 @@ user_pref("browser.tabs.warnOnClose", false);
 user_pref("browser.uitour.enabled", false);
 user_pref("browser.urlbar.clickSelectsAll", true);
 user_pref("browser.urlbar.maxRichResults", 15);
+// PREF: Don't trim HTTP off of URLs in the address bar.
+// https://bugzilla.mozilla.org/show_bug.cgi?id=665580
 user_pref("browser.urlbar.trimURLs", false);
+// PREF: Disable face detection
+user_pref("camera.control.face_detection.enabled", false);
 user_pref("datareporting.healthreport.uploadEnabled", true);
 user_pref("datareporting.policy.dataSubmissionEnabled", true);
 user_pref("datareporting.policy.firstRunURL", "");
+// PREF: Disable sensor API
+// https://wiki.mozilla.org/Sensor_API
 user_pref("device.sensors.enabled", false);
 user_pref("device.sensors.motion.enabled", false);
 user_pref("device.sensors.orientation.enabled", false);
+// PREF: Disable resource timing API
+// https://www.w3.org/TR/resource-timing/#privacy-security
+user_pref("dom.enable_resource_timing", false);
 user_pref("dom.enable_performance_observer", false);
 user_pref("dom.event.clipboardevents.enabled", false);
+// PREF: Disable FlyWeb (discovery of LAN/proximity IoT devices that expose a Web interface)
+// https://wiki.mozilla.org/FlyWeb
+// https://wiki.mozilla.org/FlyWeb/Security_scenarios
+// https://docs.google.com/document/d/1eqLb6cGjDL9XooSYEEo7mE-zKQ-o-AuDTcEyNhfBMBM/edit
+// http://www.ghacks.net/2016/07/26/firefox-flyweb
+user_pref("dom.flyweb.enabled", false);
+// PREF: Disable gamepad API to prevent USB device enumeration
+// https://www.w3.org/TR/gamepad/
+// https://trac.torproject.org/projects/tor/ticket/13023
 user_pref("dom.gamepad.enabled", false);
 user_pref("dom.gamepad.extensions.enabled", false);
+user_pref("dom.ipc.processCount", 3);
+dom.ipc.processCount// PREF: Spoof dual-core CPU
+// https://trac.torproject.org/projects/tor/ticket/21675
+// https://bugzilla.mozilla.org/show_bug.cgi?id=1360039
+user_pref("dom.maxHardwareConcurrency", 2);
 user_pref("dom.push.enabled", false);
+// PREF: Disable vibrator API
 user_pref("dom.vibrator.enabled", true);
-user_pref("experiments.activeExperiment", true);
-user_pref("experiments.enabled", true);
+// PREF: Disable virtual reality devices APIs
+// https://developer.mozilla.org/en-US/Firefox/Releases/36#Interfaces.2FAPIs.2FDOM
+// https://developer.mozilla.org/en-US/docs/Web/API/WebVR_API
+user_pref("dom.vr.enabled", false);
+// PREF: Disable the UITour backend
+// https://trac.torproject.org/projects/tor/ticket/19047#comment:3
+user_pref("browser.uitour.enabled", false);
+// PREF: Disable Mozilla experiments
+user_pref("experiments.activeExperiment", false);
+user_pref("experiments.enabled", false);
 user_pref("experiments.supported", false);
 user_pref("extensions.fxmonitor.enabled", false);
 user_pref("extensions.getAddons.cache.enabled", false);
@@ -124,16 +177,39 @@ user_pref("general.smoothScroll.scrollbars.durationMinMS", 100);
 user_pref("general.useragent.override.skype.com",
           "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36");
 user_pref("general.warnOnAboutConfig", false);
-user_pref("geo.wifi.uri", "");
+// PREF: When geolocation is enabled, don't log geolocation requests to the console
+user_pref("geo.wifi.logging.enabled", false);
+// PREF: Disable leaking network/browser connection information via Javascript
+// Network Information API provides general information about the system's connection type (WiFi, cellular, etc.)
+// https://developer.mozilla.org/en-US/docs/Web/API/Network_Information_API
+// https://wicg.github.io/netinfo/#privacy-considerations
+// https://bugzilla.mozilla.org/show_bug.cgi?id=960426
+user_pref("dom.netinfo.enabled", false);
+// PREF: When geolocation is enabled, use Mozilla geolocation service instead of Google
+// https://bugzilla.mozilla.org/show_bug.cgi?id=689252
+user_pref("geo.wifi.uri", "https://location.services.mozilla.com/v1/geolocate?key=%MOZILLA_API_KEY%")
 user_pref("gestures.enable_single_finger_input", true);
 user_pref("gfx.font_rendering.cleartype_params.rendering_mode", 5);
 user_pref("gfx.use_text_smoothing_setting", true);
 user_pref("gfx.webrender.enabled", true);
 user_pref("gfx.webrender.highlight-painted-layers", false);
 user_pref("gfx.work-around-driver-bugs", false);
+// PREF: Set Accept-Language HTTP header to en-US regardless of Firefox localization
+// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Accept-Language
 user_pref("intl.accept_languages", "en-us,en,uk,ru");
+// PREF: Don't use OS values to determine locale, force using Firefox locale setting
+// http://kb.mozillazine.org/Intl.locale.matchOS
+user_pref("intl.locale.matchOS", false);
 user_pref("intl.locale.requested", "en-US");
 user_pref("javascript.options.shared_memory", true);
+// PREF: Enable WebAssembly
+// https://webassembly.org/
+// https://en.wikipedia.org/wiki/WebAssembly
+// https://trac.torproject.org/projects/tor/ticket/21549
+user_pref("javascript.options.wasm", false);
+// PREF: Do not submit invalid URIs entered in the address bar to the default search engine
+// http://kb.mozillazine.org/Keyword.enabled
+user_pref("keyword.enabled", false);
 user_pref("layers.acceleration.force-enabled", true);
 user_pref("layers.amd-switchable-gfx.enabled", false);
 user_pref("layers.geometry.d3d11.enabled", false);
@@ -144,7 +220,18 @@ user_pref("media.autoplay.default", 1);
 user_pref("media.autoplay.enabled", false);
 user_pref("media.av1.enabled", true);
 user_pref("media.gpu-process-decoder", true);
+// PREF: Don't reveal your internal IP when WebRTC is enabled (Firefox >= 42)
+// https://wiki.mozilla.org/Media/WebRTC/Privacy
+// https://github.com/beefproject/beef/wiki/Module%3A-Get-Internal-IP-WebRTC
+user_pref("media.peerconnection.ice.no_host", true); // Firefox >= 52
 user_pref("media.videocontrols.picture-in-picture.enabled", true);
+// PREF: Disable speech recognition
+// https://dvcs.w3.org/hg/speech-api/raw-file/tip/speechapi.html
+// https://developer.mozilla.org/en-US/docs/Web/API/SpeechRecognition
+// https://wiki.mozilla.org/HTML5_Speech_API
+user_pref("media.webspeech.recognition.enable", false);
+// PREF: Disable speech synthesis
+// https://developer.mozilla.org/en-US/docs/Web/API/SpeechSynthesis
 user_pref("media.webspeech.synth.enabled", false);
 user_pref("network.allow-experiments", true);
 user_pref("network.cookie.prefsMigrated", true);
@@ -161,16 +248,21 @@ user_pref("network.warnOnAboutNetworking", false);
 user_pref("permissions.default.desktop-notification", 2);
 user_pref("permissions.default.geo", 2);
 user_pref("privacy.donottrackheader.enabled", true);
+// for jira
 user_pref("privacy.firstparty.isolate", false);
 user_pref("privacy.resistFingerprinting", true);
 user_pref("privacy.resistFingerprinting.autoDeclineNoUserInputCanvasPrompts", false);
 // enable webextensions on mozilla websites
-// user_pref("privacy.resistFingerprinting.block_mozAddonManager", true);
+user_pref("privacy.resistFingerprinting.block_mozAddonManager", true);
 user_pref("privacy.userContext.enabled", true);
 user_pref("privacy.userContext.longPressBehavior", 2);
 user_pref("privacy.userContext.ui.enabled", true);
 user_pref("reader.color_scheme", "dark");
-user_pref("reader.content_width", 9);
+user_pref("reader.content_width", 12);
+// PREF: Ensure you have a security delay when installing add-ons (milliseconds)
+// http://kb.mozillazine.org/Disable_extension_install_delay_-_Firefox
+// http://www.squarefree.com/2004/07/01/race-conditions-in-security-dialogs/
+user_pref("security.dialog_enable_delay", 1000);
 user_pref("security.ssl.errorReporting.automatic", true);
 // exclude sync of addons status enabled/disabled
 user_pref("services.sync.addons.ignoreUserEnabledChanges", true);
