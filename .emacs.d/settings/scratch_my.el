0ce906d48c7e22d105ded8b74759c052580d80fc
Vitalii Drevenchuk <vitalii.drevenchuk@cognotekt.com>
2019-08-30 15:05:19 +0200

    Emacs and tridactyl improvements

--- .emacs.d/settings/scratch_my.el
+++ .emacs.d/settings/scratch_my.el
@@ -151,33 +151,27 @@
     (global-font-lock-mode 1))
 
 ;; Russian
-(defun reverse-input-method (input-method)
-  "Build the reverse mapping of single letters from INPUT-METHOD."
-  (interactive
-  (list (read-input-method-name "Use input method (default current): ")))
-  (if (and input-method (symbolp input-method))
-      (setq input-method (symbol-name input-method)))
-  (let ((current current-input-method)
-        (modifiers '(nil (control) (meta) (control meta))))
-    (when input-method
-      (activate-input-method input-method))
-    (when (and current-input-method quail-keyboard-layout)
-      (dolist (map (cdr (quail-map)))
-        (let* ((to (car map))
-              (from (quail-get-translation
-                      (cadr map) (char-to-string to) 1)))
-          (when (and (characterp from) (characterp to))
-            (dolist (mod modifiers)
-              (define-key local-function-key-map
-                (vector (append mod (list from)))
-                (vector (append mod (list to)))))))))
-    (when input-method
-      (activate-input-method current))))
+(cl-loop
+ for from across "йцукенгшщзхїфівапролджєячсмитьбюЙЦУКЕНГШЩЗХЇФІВАПРОЛДЖ\ЄЯЧСМИТЬБЮ№"
+ for to   across "qwertyuiop[]asdfghjkl;'zxcvbnm,.QWERTYUIOP{}ASDFGHJKL:\"ZXCVBNM<>#"
+ do
+ (eval `(define-key local-function-key-map
+          (kbd ,(concat "C-"
+                        (string from)))
+          (kbd ,(concat "C-"
+                        (string to)))))
+ (eval `(define-key local-function-key-map
+          (kbd ,(concat "M-"
+                        (string from)))
+          (kbd ,(concat "M-"
+                        (string to)))))
+ (eval `(define-key local-function-key-map
+          (kbd ,(string from))
+          (kbd ,(string to)))))
 
 (defadvice read-passwd (around my-read-passwd act)
   (let ((local-function-key-map nil))
     ad-do-it))
-(reverse-input-method 'russian-typewriter)
 
 (setq max-mini-window-height      0.5
       compilation-always-kill     t)
