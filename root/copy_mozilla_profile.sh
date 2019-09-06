6afd09e03a1acc6228537c8879c1450f1d051ac4
Vitalii Drevenchuk <vitalii.drevenchuk@cognotekt.com>
2019-09-06 18:39:02 +0200

    Another debag

--- root/copy_mozilla_profile.sh
+++ root/copy_mozilla_profile.sh
@@ -6,18 +6,20 @@ firefox_profile_dir="/data/data/org.mozilla.firefox/files/mozilla/*.default"
 fenix_profile_dir="/data/data/org.mozilla.fenix/files/mozilla/*.default"
 user_files="termux/firefox/*"
 echo "$pwd"
-update_settings(){
+
+function update_settings () {
   folder=$1
   echo "$folder"
   cd $folder
   echo "$(ls -la)"
   echo ""
-  cp -R $user_files .
-  usernme=$(stat -c "%U" .)
-  grp=$(stat -c "%G" .)
-  chown -R "$usernme:$grp" .
-  echo "$(ls -la)"
+  # cp -R $user_files .
+  # usernme=$(stat -c "%U" .)
+  # grp=$(stat -c "%G" .)
+  # chown -R "$usernme:$grp" .
+  # echo "$(ls -la)"
 }
+
 if [ -d $firefox_profile_dir ]; then
   update_settings $firefox_profile_dir
 fi
