f33c1a764862a2b29d3064c9b96faea5aac1bf42
Vitalii Drevenchuk <vitalii.drevenchuk@cognotekt.com>
2019-09-06 18:27:41 +0200

    Update firefox settings copy script

--- root/copy_mozilla_profile.sh
+++ root/copy_mozilla_profile.sh
@@ -1,14 +1,29 @@
 #!/bin/sh
 
-mozilla_profile_dir="/data/data/org.mozilla.firefox/files/mozilla/*.default"
+set -e
+
+firefox_profile_dir="/data/data/org.mozilla.firefox/files/mozilla/*.default"
+fenix_profile_dir="/data/data/org.mozilla.fenix/files/mozilla/*.default"
 user_files="/storage/emulated/0/progs/termux/home/termux/firefox/*"
 
-cd $mozilla_profile_dir
-echo "$pwd"
-cp -R $user_files .
-usernme=$(stat -c "%U" .)
-grp=$(stat -c "%G" .)
-echo "$(ls -la | grep --color=auto user.js)"
-chown -R "$usernme:$grp" .
-echo "$(ls -la | grep  --color=auto user.js)"
+update_settings(){
+  folder=$1
+  echo "$folder"
+  cd $folder
+  echo "$(ls -la)"
+  echo ""
+  cp -R $user_files .
+  usernme=$(stat -c "%U" .)
+  grp=$(stat -c "%G" .)
+  chown -R "$usernme:$grp" .
+  echo "$(ls -la)"
+}
+if [ -d $firefox_profile_dir ]; then
+  update_settings $firefox_profile_dir
+fi
+
+if [ -d $fenix_profile_dir ]; then
+  update_settings $fenix_profile_dir
+fi
+
 
