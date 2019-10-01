a861bb70475bab1fa08545e509a5f2419a8058b0
Crandel <cradlemann@gmail.com>
2019-10-01 23:23:51 +0200

    vifm small fix

--- .config/vifm/install_deps.sh
+++ .config/vifm/install_deps.sh
@@ -1,13 +1,15 @@
 #! /bin/bash
 
-pacman_packages=(fzf zathura zathura-djvu zathura-pdf-poppler highlight dox2txt catdoc curlftpfs sshfs fuseiso)
+pacman_packages=(fzf zathura zathura-djvu zathura-pdf-poppler highlight feh docx2txt catdoc curlftpfs sshfs fuseiso)
 
 aur_packages=(rar2fs archivemount)
 
 for package in ${pacman_packages[@]}; do
-  echo "$package"; # pacman -S $package
+  sudo pacman -S --noconfirm $package
+  sleep 1
 done
 
 for apackage in ${aur_packages[@]}; do
-  echo "$apackage"; # yay -S $apackage
+  yay -S --noconfirm $apackage
+  sleep 1
 done
