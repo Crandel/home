d28516736c4740e5c92a08377e28c74bb8500e92
Vitalii Drevenchuk <vitalii.drevenchuk@cognotekt.com>
2019-11-28 09:57:13 +0100

    Small fixes

--- termux/copy_mozilla_profile.sh
+++ termux/copy_mozilla_profile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-folder_list=("org.mozilla.firefox" "org.mozilla.fenix" "org.mozilla.fennec_fdroid")
+folder_list=("org.mozilla.firefox" "org.mozilla.fenix" "org.mozilla.fenix.nightly" "org.mozilla.fennec_fdroid")
 
 cd termux/firefox
 cwd=$(pwd)
@@ -12,12 +12,12 @@ function update_settings () {
   folder=$1
   full_path=/data/data/$folder/files/mozilla/*.default
   if [ -d $full_path ]; then
+    echo "Current dir is $(pwd)"
     cd $full_path
-    echo "Current dir is $pwd"
-    echo "$(ls -lA | grep -i user*)"
     usernme=$(stat -c "%U" .)
     grp=$(stat -c "%G" .)
     cp -r $cwd/* .
+    echo "$(ls -lA | grep -i user*)"
     chown -R "$usernme:$grp" .
     echo "$(ls -lA | grep -i user*)"
   fi
