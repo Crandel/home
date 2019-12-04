5af3c4b48cc155784854461df8bacd0327d2a711
Vitalii Drevenchuk <vitalii.drevenchuk@cognotekt.com>
2019-12-04 12:08:31 +0100

    firefox 71 user.js improvements

--- .mozilla/firefox/user.js
+++ .mozilla/firefox/user.js
@@ -1,24 +1,30 @@
-user_pref("accessibility.force_disabled", false);
+user_pref("accessibility.force_disabled", true);
+user_pref("app.normandy.api_url", "");
 user_pref("app.normandy.enabled", false);
 user_pref("app.shield.optoutstudies.enabled", false);
+user_pref("app.update.auto", false);
 user_pref("app.update.checkInstallTime", false);
 user_pref("apz.allow_double_tap_zooming", true);
 user_pref("apz.allow_zooming", false);
 user_pref("apz.android.chrome_fling_physics.enabled", false);
 user_pref("beacon.enabled", false); // PREF: Disable "beacon" asynchronous HTTP transfers (used for analytics)
+user_pref("browser.aboutConfig.showWarning", false); // for the new HTML version [FF71+]
 user_pref("browser.bookmarks.restore_default_bookmarks", false);
 user_pref("browser.bookmarks.showMobileBookmarks", true);
-user_pref("browser.ctrlTab.previews", true);
 user_pref("browser.cache.disk.parent_directory", "/tmp/firefox");
+user_pref("browser.ctrlTab.previews", true);
 user_pref("browser.discovery.enabled", false); // "Allow Firefox to make personalized extension recommendations"
+user_pref("browser.display.use_document_fonts", 0);
 user_pref("browser.display.background_color", "2e2e31");
 user_pref("browser.download.autohideButton", false);
 user_pref("browser.download.panel.shown", true);
 user_pref("browser.fixup.hide_user_pass", true); // PREF: When browser.fixup.alternate.enabled is enabled, strip password from 'user:password@...' URLs
+user_pref("browser.helperApps.deleteTempFileOnExit", true); // remove temp files opened with an external application
+user_pref("browser.link.open_newwindow", 3); // open links targeting new windows in a new tab instead
+user_pref("browser.link.open_newwindow.restriction", 0);
 user_pref("browser.history_swipe_animation.disabled", false); // For macOS only
 user_pref("browser.in-content.dark-mode", true); // Dark mode in content pages
 user_pref("browser.library.activity-stream.enabled", true); // For macOS only
-user_pref("browser.urlbar.megabar", true); // Enable megabar
 user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.addons", false); // PREF: Disable Extension recommendations (Firefox >= 65)
 user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.features", false); // PREF: Disable Extension recommendations (Firefox >= 65)
 user_pref("browser.newtabpage.activity-stream.feeds.places", true);
@@ -35,7 +41,7 @@ user_pref("browser.newtabpage.activity-stream.telemetry.ping.endpoint", "");
 user_pref("browser.newtabpage.activity-stream.tippyTop.service.endpoint", "");
 user_pref("browser.newtabpage.activity-stream.topSitesRows", 3);
 user_pref("browser.newtabpage.enhanced", true);
-user_pref("browser.ping-centre.telemetry", true);
+user_pref("browser.ping-centre.telemetry", false);
 user_pref("browser.pocket.enabled", false); // PREF: Disable Pocket
 user_pref("browser.privatebrowsing.searchUI", false);
 user_pref("browser.safebrowsing.blockedURIs.enabled", false);
@@ -45,6 +51,7 @@ user_pref("browser.safebrowsing.downloads.remote.block_dangerous_host", false);
 user_pref("browser.safebrowsing.downloads.remote.block_potentially_unwanted", false);
 user_pref("browser.safebrowsing.downloads.remote.block_uncommon", false);
 user_pref("browser.safebrowsing.downloads.remote.enabled", false);
+user_pref("browser.safebrowsing.downloads.remote.url", "");
 user_pref("browser.safebrowsing.enabled", false);
 user_pref("browser.safebrowsing.malware.enabled", false);
 user_pref("browser.safebrowsing.phishing.enabled", false);
@@ -79,14 +86,16 @@ user_pref("browser.search.geoip.url", "");
 user_pref("browser.search.hiddenOneOffs", "Bing,Amazon.com,Twitter");
 user_pref("browser.search.openintab", true); // opens search results in new tab
 user_pref("browser.search.region", "US");
-user_pref("browser.search.widget.inNavBar", true); // Add search widget to navbar
-user_pref("browser.search.suggest.enabled", true);
+user_pref("browser.search.suggest.enabled", false);
 user_pref("browser.search.update", false);
-user_pref("browser.send_pings", true); // PREF: Enable pinging URIs specified in HTML <a> ping= attributes
+user_pref("browser.search.widget.inNavBar", true); // Add search widget to navbar
+user_pref("browser.send_pings", false); // PREF: Enable pinging URIs specified in HTML <a> ping= attributes
 user_pref("browser.send_pings.require_same_host", true); // PREF: When browser pings are enabled, only allow pinging the same host as the origin page
+user_pref("browser.sessionhistory.max_total_viewers", 18); // affects how many pages Firefox stores in such a way that they load super fast
 user_pref("browser.sessionstore.interval", 1800000);
-// user_pref("browser.sessionhistory.max_total_viewers", 18); // affects how many pages Firefox stores in such a way that they load super fast
+user_pref("browser.shell.checkDefaultBrowser", false);
 user_pref("browser.slowStartup.notificationDisabled", true);
+user_pref("browser.startup.blankWindow", false);
 user_pref("browser.startup.page", 3);
 user_pref("browser.tabs.drawInTitlebar", true);
 user_pref("browser.tabs.loadInBackground", false);
@@ -95,15 +104,19 @@ user_pref("browser.tabs.tabMinWidth", 30);
 user_pref("browser.tabs.warnOnClose", false);
 user_pref("browser.touchmode.auto", false);
 user_pref("browser.uitour.enabled", false);
+user_pref("browser.uitour.url", "");
 user_pref("browser.urlbar.autocomplete.enabled", false);
 user_pref("browser.urlbar.clickSelectsAll", true);
 user_pref("browser.urlbar.maxRichResults", 15);
+user_pref("browser.urlbar.megabar", true); // Enable megabar
+user_pref("browser.urlbar.speculativeConnect.enabled", false);
 user_pref("browser.urlbar.suggest.bookmark", true);
 user_pref("browser.urlbar.suggest.history", true);
 user_pref("browser.urlbar.suggest.searches", false);
 user_pref("browser.urlbar.trimURLs", false); // PREF: Don't trim HTTP off of URLs in the address bar.
+user_pref("browser.zoom.siteSpecific", true);
 user_pref("camera.control.face_detection.enabled", false); // PREF: Disable face detection
-user_pref("datareporting.healthreport.uploadEnabled", true);
+user_pref("datareporting.healthreport.uploadEnabled", false);
 user_pref("datareporting.policy.dataSubmissionEnabled", true);
 user_pref("datareporting.policy.firstRunURL", "");
 user_pref("device.sensors.enabled", false); // PREF: Disable sensor API
@@ -112,6 +125,7 @@ user_pref("device.sensors.orientation.enabled", false);
 user_pref("devtools.aboutdebugging.showSystemAddons", true);
 user_pref("devtools.netmonitor.features.webSockets", true);
 user_pref("devtools.onboarding.telemetry.logged", true);
+user_pref("devtools.webide.enabled", false); // [DEFAULT: false FF70+]
 user_pref("devtools.theme", "dark");
 user_pref("devtools.toolbox.splitconsoleEnabled", false);
 user_pref("dom.enable_performance_observer", false);
@@ -129,6 +143,7 @@ user_pref("dom.vibrator.enabled", false); // PREF: Disable vibrator API
 user_pref("dom.vr.enabled", false); // PREF: Disable virtual reality devices APIs
 user_pref("experiments.activeExperiment", false); // PREF: Disable Mozilla experiments
 user_pref("experiments.enabled", false);
+user_pref("experiments.manifest.uri", "");
 user_pref("experiments.supported", false);
 user_pref("extensions.fxmonitor.enabled", false);
 user_pref("extensions.getAddons.cache.enabled", false);
@@ -161,7 +176,6 @@ user_pref("general.smoothScroll.scrollbars.durationMaxMS", 100);
 user_pref("general.smoothScroll.scrollbars.durationMinMS", 100);
 user_pref("general.useragent.override.skype.com", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36");
 user_pref("general.warnOnAboutConfig", false);
-user_pref("browser.aboutConfig.showWarning", false);
 user_pref("geo.wifi.logging.enabled", false); // PREF: When geolocation is enabled, don't log geolocation requests to the console
 user_pref("geo.wifi.uri", "");
 user_pref("gestures.enable_single_finger_input", false);
@@ -177,6 +191,7 @@ user_pref("intl.locale.matchOS", false); // PREF: Don't use OS values to determi
 user_pref("intl.locale.requested", "en-US");
 user_pref("javascript.options.shared_memory", true);
 user_pref("javascript.options.wasm", true); // PREF: Enable WebAssembly
+user_pref("javascript.use_us_english_locale", true); // [HIDDEN PREF]
 user_pref("keyword.enabled", true); // PREF: Submit invalid URIs entered in the address bar to the default search engine
 user_pref("layers.acceleration.force-enabled", true);
 user_pref("layers.amd-switchable-gfx.enabled", false); // AMD specific settings
@@ -184,8 +199,8 @@ user_pref("layers.geometry.d3d11.enabled", false); // Windows specific settings
 user_pref("layers.gpu-process.enabled", true);
 user_pref("layout.css.devPixelsPerPx", "1.25");
 user_pref("layout.css.osx-font-smoothing.enabled", true);
-user_pref("layout.word_select.stop_at_punctuation", true);
 user_pref("layout.spellcheckDefault", 2); // enable spell-check for all text boxes
+user_pref("layout.word_select.stop_at_punctuation", true);
 user_pref("lightweightThemes.selectedThemeID", "firefox-compact-dark@mozilla.org");
 user_pref("media.autoplay.allow-muted", false);
 user_pref("media.autoplay.default", 0);
@@ -199,14 +214,19 @@ user_pref("media.videocontrols.picture-in-picture.video-toggle.enabled", true);
 user_pref("media.videocontrols.picture-in-picture.video-toggle.flyout-enabled", true);
 user_pref("media.webspeech.recognition.enable", false); // PREF: Disable speech recognition
 user_pref("media.webspeech.synth.enabled", false); // PREF: Disable speech synthesis
+user_pref("middlemouse.paste", true);
 user_pref("mousewheel.min_line_scroll_amount", 36);
-user_pref("network.allow-experiments", true);
+user_pref("network.allow-experiments", false);
 user_pref("network.cookie.prefsMigrated", true);
 user_pref("network.dns.disablePrefetch", false);
 user_pref("network.http.altsvc.enabled", false); // https://www.usenix.org/conference/woot19/presentation/tiwari
+user_pref("network.http.altsvc.oe", false);
+user_pref("network.http.redirection-limit", 10);
 user_pref("network.http.speculative-parallel-limit", 0);
+user_pref("network.predictor.enable-prefetch", false);
 user_pref("network.predictor.enabled", false);
 user_pref("network.prefetch-next", false); // After the browser is finished loading the page, it begins silently prefetching specified documents and stores them in its cache
+user_pref("network.proxy.socks_remote_dns", false);
 user_pref("network.security.esni.enabled", true);
 user_pref("network.stricttransportsecurity.preloadlist", true);
 user_pref("network.tcp.tcp_fastopen_enable", true);
@@ -222,6 +242,7 @@ user_pref("privacy.firstparty.isolate", false); // for jira
 user_pref("privacy.resistFingerprinting", true);
 user_pref("privacy.resistFingerprinting.autoDeclineNoUserInputCanvasPrompts", false); // Add dialog for canvas API
 user_pref("privacy.resistFingerprinting.block_mozAddonManager", true); // enable webextensions on mozilla websites
+user_pref("privacy.resistFingerprinting.letterboxing", false);
 user_pref("privacy.userContext.enabled", true); // Multicontainer enables
 user_pref("privacy.userContext.longPressBehavior", 2);
 user_pref("privacy.userContext.ui.enabled", true);
@@ -229,7 +250,9 @@ user_pref("reader.color_scheme", "dark");
 user_pref("reader.content_width", 12);
 user_pref("security.dialog_enable_delay", 1000); // PREF: Ensure you have a security delay when installing add-ons (milliseconds)
 user_pref("security.secure_connection_icon_color_gray", false); // Return green lock for https (Firefox >= 70)
-user_pref("security.ssl.errorReporting.automatic", true);
+user_pref("security.ssl.errorReporting.automatic", false);
+user_pref("security.ssl.errorReporting.enabled", false);
+user_pref("security.ssl.errorReporting.url", "");
 user_pref("services.sync.addons.ignoreUserEnabledChanges", true); // exclude sync of addons status enabled/disabled
 user_pref("services.sync.prefs.sync.browser.newtabpage.activity-stream.section.highlights.includePocket", false);
 user_pref("services.sync.prefs.sync.browser.newtabpage.enabled", false); // exclude sync newtabpage icons
