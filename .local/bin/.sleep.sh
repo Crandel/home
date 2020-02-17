334bef0335e528d78a49387ff5ec2f1068292b91
Crandel <cradlemann@gmail.com>
2020-02-17 18:13:19 +0100

    sleep settings

--- .local/bin/.sleep.sh
+++ .local/bin/.sleep.sh
@@ -1,4 +1,7 @@
 #! /bin/bash
-if [ ! $(pgrep "i3lock" > /dev/null) ]; then
-    i3lock -c 000000 -n
+if ! pgrep "i3lock" > /dev/null ; then
+  dbus-send --type=method_call --dest=ru.gentoo.KbddService /ru/gentoo/KbddService ru.gentoo.kbdd.set_layout uint32:2
+  echo "Succesfully block screen"
+  i3lock -c 000000 -n
+  echo "i3lock was closed"
 fi
