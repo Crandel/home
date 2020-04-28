569a77603f0d84b96e4b2b7d7dbe136b3441a9de
Crandel <cradlemann@gmail.com>
2020-04-28 13:49:29 +0200

    sway improvements

--- .local/bin/.sleep.sh
+++ .local/bin/.sleep.sh
@@ -1,7 +1,12 @@
 #! /bin/bash
-if ! pgrep "i3lock" > /dev/null ; then
-  dbus-send --type=method_call --dest=ru.gentoo.KbddService /ru/gentoo/KbddService ru.gentoo.kbdd.set_layout uint32:2
+# if hash i3lock >/dev/null 2>&1 && ! pgrep "i3lock" > /dev/null ; then
+#   dbus-send --type=method_call --dest=ru.gentoo.KbddService /ru/gentoo/KbddService ru.gentoo.kbdd.set_layout uint32:2
+#   echo "Succesfully block screen"
+#   i3lock -c 000000 -n
+#   echo "i3lock was closed"
+# fi
+if hash swaylock >/dev/null 2>&1 && ! pgrep "swaylock" > /dev/null ; then
   echo "Succesfully block screen"
-  i3lock -c 000000 -n
+  swaylock -c 000000 -n
   echo "i3lock was closed"
 fi
--- .local/share/systemd/user/drive_sync.sh
+++ .local/share/systemd/user/drive_sync.sh
@@ -8,5 +8,5 @@ grive -s <sync> \
 
 retVal=$?
 if [ $retVal -ne 0 ]; then
-  notify-send --urgency=critical 'Grive service' "Failed with service code $retVal"
+  notify-send --urgency=critical 'Drive service' "Failed with service code $retVal"
 fi
