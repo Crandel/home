812ebe6a0c53a549ac13158af405d87f70ba047a
Crandel <cradlemann@gmail.com>
2020-05-20 08:50:42 +0200

    cargo aliases & lock opts

--- .local/bin/sleep.sh
+++ .local/bin/sleep.sh
@@ -4,14 +4,14 @@ if ! pgrep "sway" ; then
   if hash i3lock >/dev/null 2>&1 && ! pgrep "i3lock" > /dev/null ; then
     dbus-send --type=method_call --dest=ru.gentoo.KbddService /ru/gentoo/KbddService ru.gentoo.kbdd.set_layout uint32:2
     echo "Succesfully block screen using i3lock"
-    i3lock -c 000000 -n
+    i3lock -c 000000 -n -e -f
     echo "i3lock was closed"
   fi
 else
   if hash swaylock >/dev/null 2>&1 && ! pgrep "swaylock" > /dev/null ; then
     swaymsg input type:keyboard xkb_switch_layout 0
     echo "Succesfully block screen using swaylock"
-    swaylock -c 000000 -n
+    swaylock -c 000000 -F -e -k -l --font Hack --font-size 22
     echo "swaylock was closed"
   fi
 fi
