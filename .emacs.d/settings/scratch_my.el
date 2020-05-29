fdb3992b685321e15cb5f3145a4d4ead92615a0c
Crandel <cradlemann@gmail.com>
2020-05-29 12:38:31 +0200

    emacs sway copy-paste

--- .emacs.d/settings/scratch_my.el
+++ .emacs.d/settings/scratch_my.el
@@ -104,6 +104,21 @@
 
 ;; Clipboard settings
 (setq select-enable-clipboard t)
+(setq wl-copy-process nil)
+(defun wl-copy (text)
+  (setq wl-copy-process (make-process :name "wl-copy"
+                                      :buffer nil
+                                      :command '("wl-copy" "-f" "-n")
+                                      :connection-type 'pipe))
+  (process-send-string wl-copy-process text)
+  (process-send-eof wl-copy-process))
+(defun wl-paste ()
+  (if (and wl-copy-process (process-live-p wl-copy-process))
+      nil ; should return nil if we're the current paste owner
+    (shell-command-to-string "wl-paste -n | tr -d \r")))
+(setq interprogram-cut-function 'wl-copy)
+(setq interprogram-paste-function 'wl-paste)
+;; End sway clipboard settings
 
 (setq next-line-add-newlines nil)
 
