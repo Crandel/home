51993e0f11a6aca02eb89409e5da86c91403c816
Crandel <cradlemann@gmail.com>
2020-07-03 23:30:39 +0200

    Add timestamp, termux copy firefox

--- termux/copy_mozilla_profile.sh
+++ termux/copy_mozilla_profile.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-folder_list=("org.mozilla.firefox" "org.mozilla.fenix" "org.mozilla.fennec_aurora" "org.mozilla.fennec_fdroid")
+folder_list=(
+  "org.mozilla.fenix"
+  "org.mozilla.fennec_fdroid"
+  "org.mozilla.firefox_beta"
+)
 
 cd termux/firefox
 cwd=$(pwd)
@@ -11,18 +15,18 @@ function update_settings () {
   full_path=/data/data/$folder/files/mozilla/*.default
   if [ -d $full_path ]; then
     cd $full_path
-    echo "Current dir is $(pwd)"
+    echo "Current dir is $(pwd)\n"
     usernme=$(stat -c "%U" .)
     grp=$(stat -c "%G" .)
     cp -r $cwd/* .
-    echo "Before chown $(ls -lA | grep -i user)"
+    echo "Before chown $(ls -lA | grep -i user)\n"
     chown -R "$usernme:$grp" .
-    echo "After chown $(ls -lA | grep -i user)"
+    echo "After chown $(ls -lA | grep -i user)\n"
   fi
 }
 
 for folder in "${folder_list[@]}"; do
-  echo "$folder"
+  echo "Current folder is $folder"
   update_settings $folder
 done
 
