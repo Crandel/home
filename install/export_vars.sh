bef4a2283f7f89af1fe090fe71392b77439bb0f1
Crandel <cradlemann@gmail.com>
2020-09-01 14:00:12 +0200

    Add wine config

--- install/export_vars.sh
+++ install/export_vars.sh
@@ -4,3 +4,7 @@ export yay='yay --aur --editmenu --builddir /opt/work/bb'
 if [[ $EUID -ne 0 ]] && command -v "sudo"  > /dev/null 2>&1; then
   export pacman="sudo pacman"
 fi
+
+function command_exists () {
+  command -v "$1"  > /dev/null 2>&1;
+}
---
+++ install/install_wine.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/bash
+
+source export_vars.sh
+
+if ! command_exists wine ; then
+  $pacman -S wine \
+             winetricks \
+             wine-mono \
+             wine-gecko \
+             wine-nine
+fi
+
+WINEARCH=win32 winetricks win7 sound=pulse psm=3 mwo=enabled multisampling=enabled mimeassoc=off macdriver=x11 isolate_home hosts heapcheck gsm=3 glsl=enabled fontsmooth=rgb fontfix ddr=opengl cfc=enabled
+
+fonts=(corefonts tahoma georgia droid courier consolas arial)
+
+for font in "${fonts[@]}"; do
+  echo "Apply font: $font"
+  winetricks $font
+  echo "Finish -------------------"
+done
+
+dlls=(d3dcompiler_47 d3dx10 d3dx11_43 d3dx9 d9vk dinput8 dmusic dotnet48 dxvk galliumnine msxml6 python27 vb6run vcrun2019)
+
+for dll in "${dlls[@]}"; do
+  echo "Apply dll: $dll"
+  winetricks $dll
+  echo "Finish -------------------"
+done
