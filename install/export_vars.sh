5fb76af3327e817be87c5579004fe74565a2f816
Crandel <cradlemann@gmail.com>
2020-09-11 13:44:13 +0200

    fix typo in export_vars

--- install/export_vars.sh
+++ install/export_vars.sh
@@ -1,10 +1,18 @@
 mkdir -p /opt/work/bb
-export yay='yay --aur --editmenu --builddir /opt/work/bb'
+export yay='yay --aur --editmenu --builddir /data/work/bb'
 
 if [[ $EUID -ne 0 ]] && command -v "sudo"  > /dev/null 2>&1; then
   export pacman="sudo pacman"
 fi
 
+
 function command_exists () {
   command -v "$1"  > /dev/null 2>&1;
 }
+
+if ! command_exists yay ; then
+  $pacman -S git
+  git clone https://aur.archlinux.org/yay.git /tmp/yay
+  cd /tmp/yay
+  makepkg -si
+fi
--- install/install_aur.sh
+++ install/install_aur.sh
@@ -2,13 +2,6 @@
 
 source export_vars.sh
 
-if ! command_exists yay ; then
-  $pacman -S git
-  git clone https://aur.archlinux.org/yay.git /tmp/yay
-  cd /tmp/yay
-  makepkg -si
-fi
-
 $yay -S adb-sync-git \
         grive-git \
         inxi \
