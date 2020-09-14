39bcdd22fce4b6761ab8f7ca500b4d4ca7a7cd0e
Crandel <cradlemann@gmail.com>
2020-09-14 11:23:14 +0200

    Add desktop sharing sway

--- install/8.install_media.sh
+++ install/8.install_media.sh
@@ -14,14 +14,27 @@ $pacman -S dav1d \
            leptonica \
            libjpeg-turbo \
            libva-intel-driver \
+           lib32-libpulse \
            mp3info \
            mpv \
            nomacs \
            opus \
            opusfile \
+           pipewire \
+           pipewire-jack \
+           pipewire-pulse \
            png++ \
+           pulseaudio \
+           pulseaudio-bluetooth \
+           pulseaudio-jack \
+           pulsemixer \
            rav1e \
            vlc \
+           xdg-desktop-portal \
+           xdg-desktop-portal-gtk \
+           xdg-desktop-portal-kde \
            zathura \
            zathura-djvu \
            zathura-pdf-mupdf
+
+$yay -S xdg-desktop-portal-wlr
--- install/9.install_langs.sh
+++ install/9.install_langs.sh
@@ -3,17 +3,20 @@
 source export_vars.sh
 
 $pacman -S bpython \
+           flake8 \
            helm \
            jre11-openjdk \
            kubectl \
            kubectx \
            openjdk11-doc \
            openjdk11-src \
+           python-rope \
            rust \
            sbt \
            scala \
            scala-docs \
-           scala-sources
+           scala-sources \
+           yarf
 
 $yay -S coursier \
         k9s \
