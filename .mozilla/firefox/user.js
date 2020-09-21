22965fe00b7a349d166d9b43616cf70d64196bc1
Crandel <cradlemann@gmail.com>
2020-09-21 21:37:18 +0200

    moz changes

--- .mozilla/firefox/user.js
+++ .mozilla/firefox/user.js
@@ -155,7 +155,7 @@ user_pref("dom.flyweb.enabled", false); // PREF: Disable FlyWeb (discovery of LA
 user_pref("dom.gamepad.enabled", false); // PREF: Disable gamepad API to prevent USB device enumeration
 user_pref("dom.gamepad.extensions.enabled", false);
 user_pref("dom.image-lazy-loading.enabled", true);
-user_pref("dom.ipc.processCount", 3);
+user_pref("dom.ipc.processCount", 8);
 user_pref("dom.maxHardwareConcurrency", 3); // PREF: Spoof dual-core CPU
 user_pref("dom.max_script_run_time", 30); // PREF: script execution time
 user_pref("dom.netinfo.enabled", false); // PREF: Disable leaking network/browser connection information via Javascript
