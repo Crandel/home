bc3b9cfe0b7764519a0a91283cdc0ccd9de18cc1
Crandel <cradlemann@gmail.com>
2020-11-03 16:08:09 +0100

    Add tfswitch to install

--- install/9.install_langs.sh
+++ install/9.install_langs.sh
@@ -2,23 +2,27 @@
 
 source export_vars.sh
 
-$pacman -S bpython \
-           flake8 \
-           helm \
-           jre11-openjdk \
-           kubectl \
-           kubectx \
-           openjdk11-doc \
-           openjdk11-src \
-           python-rope \
-           rust \
-           sbt \
-           scala \
-           scala-docs \
-           scala-sources \
-           yarf
+$pacman -S \
+        bpython \
+        flake8 \
+        helm \
+        jre11-openjdk \
+        kubectl \
+        kubectx \
+        openjdk11-doc \
+        openjdk11-src \
+        python-rope \
+        rust \
+        sbt \
+        scala \
+        scala-docs \
+        scala-sources \
+        yarf
 
-$yay -S coursier \
-        k9s \
-        litecli \
-        pgcli
+$yay -S \
+     coursier \
+     k9s \
+     litecli \
+     mycli \
+     pgcli \
+     tfswitch
