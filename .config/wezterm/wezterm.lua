feb4d2e044038796a269ed3ff464765786bcf014
Crandel <cradlemann@gmail.com>
2021-05-24 07:54:07 +0200

    tab wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -7,11 +7,13 @@ return {
    font_size=18.0,
    hide_tab_bar_if_only_one_tab = true,
    keys = {
-      {key="Z", mods="CTRL", action=wezterm.action{SplitVertical={domain="CurrentPaneDomain"}}},
-      {key="X", mods="CTRL", action=wezterm.action{SplitHorizontal={domain="CurrentPaneDomain"}}},
-      {key="z", mods="CTRL", action="Nop"},
-      {key="t", mods="CTRL", action=wezterm.action{SpawnTab="CurrentPaneDomain"}},
-      {key="A", mods="CTRL", action="ActivateCopyMode"}
+      {key="Z",   mods="CTRL", action=wezterm.action{SplitVertical={domain="CurrentPaneDomain"}}},
+      {key="X",   mods="CTRL", action=wezterm.action{SplitHorizontal={domain="CurrentPaneDomain"}}},
+      {key="z",   mods="CTRL", action="Nop"},
+      {key="t",   mods="CTRL", action=wezterm.action{SpawnTab="CurrentPaneDomain"}},
+      {key="Tab", mods="CTRL", action=wezterm.action{ActivateTabRelative=1}},
+      {key="Tab", mods="CTRL|SHIFT", action=wezterm.action{ActivateTabRelative=-1}},
+      {key="A",   mods="CTRL", action="ActivateCopyMode"}
    },
    ratelimit_output_bytes_per_second = 4289999998,
    scrollback_lines = 150000,
