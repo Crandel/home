91e954a8f006617909cb35b85eaa75f082b118bd
Crandel <cradlemann@gmail.com>
2021-08-31 00:46:32 +0200

    vieb hw video

--- .local/bin/vieb.sh
+++ .local/bin/vieb.sh
@@ -1,3 +1,3 @@
 ##!/usr/bin/env bash
 
-/opt/Vieb/vieb --force_low_power_gpu --enable-features=UseOzonePlatform,WebRTCPipeWireCapturer,WebContentsForceDark --ozone-platform=wayland
+/opt/Vieb/vieb --force_low_power_gpu --ignore-gpu-blacklist --enable-features=UseOzonePlatform,WebRTCPipeWireCapturer,WebContentsForceDark,VaapiVideoDecoder --ozone-platform=wayland
