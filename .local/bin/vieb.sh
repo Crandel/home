070b5133678bf91b8e1ff4bba0b13bd994654d65
Crandel <cradlemann@gmail.com>
2021-08-31 00:40:58 +0200

    vieb

--- .local/bin/vieb.sh
+++ .local/bin/vieb.sh
@@ -1,3 +1,3 @@
 ##!/usr/bin/env bash
 
-/opt/Vieb/vieb  --enable-features=UseOzonePlatform,WebRTCPipeWireCapturer --ozone-platform=wayland
+/opt/Vieb/vieb --force_low_power_gpu --enable-features=UseOzonePlatform,WebRTCPipeWireCapturer,WebContentsForceDark --ozone-platform=wayland
