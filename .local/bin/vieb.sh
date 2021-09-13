36d11863f168b43972f9a61b608a9693cb85b1c8
Crandel <cradlemann@gmail.com>
2021-09-13 23:38:25 +0200

    vieb

--- .local/bin/vieb.sh
+++ .local/bin/vieb.sh
@@ -1,6 +1,7 @@
 ##!/usr/bin/env bash
 
 /opt/Vieb/vieb --use-cmd-decoder=validating \
+               --site-isolation=strict \
                --force_low_power_gpu \
                --ignore-gpu-blacklist \
                --enable-features=UseOzonePlatform,WebRTCPipeWireCapturer,WebContentsForceDark,VaapiVideoDecoder \
